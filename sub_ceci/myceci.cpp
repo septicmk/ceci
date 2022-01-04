e7bade4b6c88f0772f291619a3b9a1cef22caeb1
KeweiZhang <zhangkewei1994@gmail.com>
2022-01-04 15:31:50 +0800

    upset code tailor.

--- sub_ceci/myceci.cpp
+++ sub_ceci/myceci.cpp
@@ -3,9 +3,12 @@
 #include "matchingcommand.h"
 #include "BuildTable.h"
 #include <memory.h>
-#include "myceci.h"
+#include "utility/computesetintersection.h"
 #include <queue>
+#include "myceci.h"
 #define INVALID_VERTEX_ID 100000000
+#define NANOSECTOSEC(elapsed_time) ((elapsed_time)/(double)1000000000)
+#define BYTESTOMB(memory_cost) ((memory_cost)/(double)(1024 * 1024))
 
 VertexID selectCECIStartVertex(const Graph *data_graph, const Graph *query_graph) {
     double min_score = data_graph->getVerticesCount();
@@ -157,6 +160,22 @@ void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, 
         }
     }
 
+}
+
+void compactCandidates(ui **&candidates, ui *&candidates_count, ui query_vertex_num) {
+    for (ui i = 0; i < query_vertex_num; ++i) {
+        VertexID query_vertex = i;
+        ui next_position = 0;
+        for (ui j = 0; j < candidates_count[query_vertex]; ++j) {
+            VertexID data_vertex = candidates[query_vertex][j];
+
+            if (data_vertex != INVALID_VERTEX_ID) {
+                candidates[query_vertex][next_position++] = data_vertex;
+            }
+        }
+
+        candidates_count[query_vertex] = next_position;
+    }
 }
 
 bool CECIFilter(const Graph *data_graph, const Graph *query_graph, ui **&candidates, ui *&candidates_count,
@@ -341,51 +360,153 @@ bool CECIFilter(const Graph *data_graph, const Graph *query_graph, ui **&candida
             }
         }
     }
+    // Reverse BFS refine.
+    std::vector<std::vector<ui>> cardinality(query_vertices_count);
+    for (ui i = 0; i < query_vertices_count; ++i) {
+        cardinality[i].resize(candidates_count[i], 1);
+    }
 
-void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex, ui &count, ui *buffer) {
-    LabelID label = query_graph->getVertexLabel(query_vertex);
-    ui degree = query_graph->getVertexDegree(query_vertex);
-#if OPTIMIZED_LABELED_GRAPH == 1
-    const std::unordered_map<LabelID, ui>* query_vertex_nlf = query_graph->getVertexNLF(query_vertex);
-#endif
-    ui data_vertex_num;
-    const ui* data_vertices = data_graph->getVerticesByLabel(label, data_vertex_num);
-    count = 0;
-    for (ui j = 0; j < data_vertex_num; ++j) {
-        ui data_vertex = data_vertices[j];
-        if (data_graph->getVertexDegree(data_vertex) >= degree) {
+    std::vector<ui> local_cardinality(data_vertices_count);
+    std::fill(local_cardinality.begin(), local_cardinality.end(), 0);
 
-            // NFL check
-#if OPTIMIZED_LABELED_GRAPH == 1
-            const std::unordered_map<LabelID, ui>* data_vertex_nlf = data_graph->getVertexNLF(data_vertex);
+    for (int i = query_vertices_count - 1; i >= 0; --i) {
+        VertexID u = order[i];
+        TreeNode& u_node = tree[u];
 
-            if (data_vertex_nlf->size() >= query_vertex_nlf->size()) {
-                bool is_valid = true;
+        ui flag_num = 0;
+        ui updated_flag_count = 0;
 
-                for (auto element : *query_vertex_nlf) {
-                    auto iter = data_vertex_nlf->find(element.first);
-                    if (iter == data_vertex_nlf->end() || iter->second < element.second) {
-                        is_valid = false;
-                        break;
+        // Compute the intersection of TE_Candidates and NTE_Candidates.
+        for (ui j = 0; j < candidates_count[u]; ++j) {
+            VertexID v = candidates[u][j];
+
+            if (v == INVALID_VERTEX_ID)
+                continue;
+
+            if (flag[v] == flag_num) {
+                flag[v] += 1;
+                updated_flag[updated_flag_count++] = v;
+            }
+        }
+
+        for (ui j = 0; j < u_node.bn_count_; ++j) {
+            VertexID u_bn = u_node.bn_[j];
+            flag_num += 1;
+            for (auto iter = NTE_Candidates[u][u_bn].begin(); iter != NTE_Candidates[u][u_bn].end(); ++iter) {
+                for (auto v : iter->second) {
+                    if (flag[v] == flag_num) {
+                        flag[v] += 1;
                     }
                 }
+            }
+        }
 
-                if (is_valid) {
-                    if (buffer != NULL) {
-                        buffer[count] = data_vertex;
+        flag_num += 1;
+
+        // Get the cardinality of the candidates of u.
+        for (ui j = 0; j < candidates_count[u]; ++j) {
+            VertexID v = candidates[u][j];
+            if (v != INVALID_VERTEX_ID && flag[v] == flag_num) {
+                local_cardinality[v] = cardinality[u][j];
+            }
+            else {
+                cardinality[u][j] = 0;
+            }
+        }
+
+        VertexID u_p = u_node.parent_;
+        VertexID* frontiers = candidates[u_p];
+        ui frontiers_count = candidates_count[u_p];
+
+        // Loop over TE_Candidates.
+        for (ui j = 0; j < frontiers_count; ++j) {
+            VertexID v_f = frontiers[j];
+
+            if (v_f == INVALID_VERTEX_ID) {
+                cardinality[u_p][j] = 0;
+                continue;
+            }
+
+            ui temp_score = 0;
+            for (auto iter = TE_Candidates[u][v_f].begin(); iter != TE_Candidates[u][v_f].end();) {
+                VertexID v = *iter;
+                temp_score += local_cardinality[v];
+                if (local_cardinality[v] == 0) {
+                    iter = TE_Candidates[u][v_f].erase(iter);
+                    for (ui k = 0; k < u_node.children_count_; ++k) {
+                        VertexID u_c = u_node.children_[k];
+                        TE_Candidates[u_c].erase(v);
                     }
-                    count += 1;
+
+                    for (ui k = 0; k < u_node.fn_count_; ++k) {
+                        VertexID u_c = u_node.fn_[k];
+                        NTE_Candidates[u_c][u].erase(v);
+                    }
+                }
+                else {
+                    ++iter;
                 }
             }
-#else
-            if (buffer != NULL) {
-                buffer[count] = data_vertex;
-            }
-            count += 1;
-#endif
+
+            cardinality[u_p][j] *= temp_score;
+        }
+
+        // Clear updated flag.
+        for (ui j = 0; j < updated_flag_count; ++j) {
+            flag[updated_flag[j]] = 0;
+            local_cardinality[updated_flag[j]] = 0;
         }
     }
 
+    compactCandidates(candidates, candidates_count, query_vertices_count);
+    sortCandidates(candidates, candidates_count, query_vertices_count);
+
+
+    for (ui i = 0; i < query_vertices_count; ++i) {
+        if (candidates_count[i] == 0) {
+            return false;
+        }
+    }
+
+    for (ui i = 1; i < query_vertices_count; ++i) {
+        VertexID u = order[i];
+        TreeNode& u_node = tree[u];
+
+        // Clear TE_Candidates.
+        {
+            VertexID u_p = u_node.parent_;
+            auto iter = TE_Candidates[u].begin();
+            while (iter != TE_Candidates[u].end()) {
+                VertexID v_f = iter->first;
+                if (!std::binary_search(candidates[u_p], candidates[u_p] + candidates_count[u_p], v_f)) {
+                    iter = TE_Candidates[u].erase(iter);
+                }
+                else {
+                    std::sort(iter->second.begin(), iter->second.end());
+                    iter++;
+                }
+            }
+        }
+
+        // Clear NTE_Candidates.
+        {
+            for (ui j = 0; j < u_node.bn_count_; ++j) {
+                VertexID u_p = u_node.bn_[j];
+                auto iter = NTE_Candidates[u][u_p].end();
+                while (iter != NTE_Candidates[u][u_p].end()) {
+                    VertexID v_f = iter->first;
+                    if (!std::binary_search(candidates[u_p], candidates[u_p] + candidates_count[u_p], v_f)) {
+                        iter = NTE_Candidates[u][u_p].erase(iter);
+                    }
+                    else {
+                        std::sort(iter->second.begin(), iter->second.end());
+                        iter++;
+                    }
+                }
+            }
+        }
+    }
+        return true;
 }
 
 void generateCECIQueryPlan(const Graph *query_graph, TreeNode *tree, ui *bfs_order, ui *&order,
@@ -403,6 +524,190 @@ void generateCECIQueryPlan(const Graph *query_graph, TreeNode *tree, ui *bfs_ord
     }
 }
 
+void sortCandidates(ui **candidates, ui *candidates_count, ui num) {
+    for (ui i = 0; i < num; ++i) {
+        std::sort(candidates[i], candidates[i] + candidates_count[i]);
+    }
+}
+
+size_t exploreCECIStyle(const Graph *data_graph, const Graph *query_graph, TreeNode *tree, ui **candidates,
+                                ui *candidates_count,
+                                std::vector<std::unordered_map<VertexID, std::vector<VertexID>>> &TE_Candidates,
+                                std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates,
+                                ui *order, size_t &output_limit_num, size_t &call_count) {
+
+    int max_depth = query_graph->getVerticesCount();
+    ui data_vertices_count = data_graph->getVerticesCount();
+    ui max_valid_candidates_count = 0;
+    for (int i = 0; i < max_depth; ++i) {
+        if (candidates_count[i] > max_valid_candidates_count) {
+            max_valid_candidates_count = candidates_count[i];
+        }
+    }
+    // Allocate the memory buffer.
+    ui *idx = new ui[max_depth];
+    ui *idx_count = new ui[max_depth];
+    ui *embedding = new ui[max_depth];
+    ui *temp_buffer = new ui[max_valid_candidates_count];
+    ui **valid_candidates = new ui *[max_depth];
+    for (int i = 0; i < max_depth; ++i) {
+        valid_candidates[i] = new ui[max_valid_candidates_count];
+    }
+    bool *visited_vertices = new bool[data_vertices_count];
+    std::fill(visited_vertices, visited_vertices + data_vertices_count, false);
+
+    // Evaluate the query.
+    size_t embedding_cnt = 0;
+    int cur_depth = 0;
+    VertexID start_vertex = order[0];
+
+    idx[cur_depth] = 0;
+    idx_count[cur_depth] = candidates_count[start_vertex];
+
+    for (ui i = 0; i < idx_count[cur_depth]; ++i) {
+        valid_candidates[cur_depth][i] = candidates[start_vertex][i];
+    }
+
+#ifdef ENABLE_FAILING_SET
+    std::vector<std::bitset<MAXIMUM_QUERY_GRAPH_SIZE>> ancestors;
+    computeAncestor(query_graph, order, ancestors);
+    std::vector<std::bitset<MAXIMUM_QUERY_GRAPH_SIZE>> vec_failing_set(max_depth);
+    std::unordered_map<VertexID, VertexID> reverse_embedding;
+    reverse_embedding.reserve(MAXIMUM_QUERY_GRAPH_SIZE * 2);
+#endif
+
+    while (true) {
+        while (idx[cur_depth] < idx_count[cur_depth]) {
+            VertexID u = order[cur_depth];
+            VertexID v = valid_candidates[cur_depth][idx[cur_depth]];
+            idx[cur_depth] += 1;
+
+            if (visited_vertices[v]) {
+#ifdef ENABLE_FAILING_SET
+                vec_failing_set[cur_depth] = ancestors[u];
+                vec_failing_set[cur_depth] |= ancestors[reverse_embedding[v]];
+                vec_failing_set[cur_depth - 1] |= vec_failing_set[cur_depth];
+#endif
+                continue;
+            }
+
+            embedding[u] = v;
+            visited_vertices[v] = true;
+
+#ifdef ENABLE_FAILING_SET
+            reverse_embedding[v] = u;
+#endif
+            if (cur_depth == max_depth - 1) {
+                embedding_cnt += 1;
+                visited_vertices[v] = false;
+#ifdef ENABLE_FAILING_SET
+                reverse_embedding.erase(embedding[u]);
+                vec_failing_set[cur_depth].set();
+                vec_failing_set[cur_depth - 1] |= vec_failing_set[cur_depth];
+#endif
+                if (embedding_cnt >= output_limit_num) {
+                    goto EXIT;
+                }
+            } else {
+                call_count += 1;
+                cur_depth += 1;
+                idx[cur_depth] = 0;
+                generateValidCandidates(cur_depth, embedding, idx_count, valid_candidates, order, temp_buffer, tree,
+                                        TE_Candidates,
+                                        NTE_Candidates);
+#ifdef ENABLE_FAILING_SET
+                if (idx_count[cur_depth] == 0) {
+                    vec_failing_set[cur_depth - 1] = ancestors[order[cur_depth]];
+                } else {
+                    vec_failing_set[cur_depth - 1].reset();
+                }
+#endif
+            }
+        }
+
+        cur_depth -= 1;
+        if (cur_depth < 0)
+            break;
+        else {
+            VertexID u = order[cur_depth];
+#ifdef ENABLE_FAILING_SET
+            reverse_embedding.erase(embedding[u]);
+            if (cur_depth != 0) {
+                if (!vec_failing_set[cur_depth].test(u)) {
+                    vec_failing_set[cur_depth - 1] = vec_failing_set[cur_depth];
+                    idx[cur_depth] = idx_count[cur_depth];
+                } else {
+                    vec_failing_set[cur_depth - 1] |= vec_failing_set[cur_depth];
+                }
+            }
+#endif
+            visited_vertices[embedding[u]] = false;
+        }
+    }
+
+    // Release the buffer.
+    EXIT:
+    delete[] idx;
+    delete[] idx_count;
+    delete[] embedding;
+    delete[] temp_buffer;
+    delete[] visited_vertices;
+    for (int i = 0; i < max_depth; ++i) {
+        delete[] valid_candidates[i];
+    }
+    delete[] valid_candidates;
+
+    return embedding_cnt;
+}
+
+void generateValidCandidates(ui depth, ui *embedding, ui *idx_count, ui **valid_candidates, ui *order,
+                                            ui *&temp_buffer, TreeNode *tree,
+                                            std::vector<std::unordered_map<VertexID, std::vector<VertexID>>> &TE_Candidates,
+                                            std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates) {
+
+    VertexID u = order[depth];
+    TreeNode &u_node = tree[u];
+    idx_count[depth] = 0;
+    ui valid_candidates_count = 0;
+    {
+        VertexID u_p = tree[u].parent_;
+        VertexID v_p = embedding[u_p];
+
+        auto iter = TE_Candidates[u].find(v_p);
+        if (iter == TE_Candidates[u].end() || iter->second.empty()) {
+            return;
+        }
+
+        valid_candidates_count = iter->second.size();
+        VertexID *v_p_nbrs = iter->second.data();
+
+        for (ui i = 0; i < valid_candidates_count; ++i) {
+            valid_candidates[depth][i] = v_p_nbrs[i];
+        }
+    }
+    ui temp_count;
+    for (ui i = 0; i < tree[u].bn_count_; ++i) {
+        VertexID u_p = tree[u].bn_[i];
+        VertexID v_p = embedding[u_p];
+
+        auto iter = NTE_Candidates[u][u_p].find(v_p);
+        if (iter == NTE_Candidates[u][u_p].end() || iter->second.empty()) {
+            return;
+        }
+
+        ui current_candidates_count = iter->second.size();
+        ui *current_candidates = iter->second.data();
+
+        ComputeSetIntersection::ComputeCandidates(current_candidates, current_candidates_count,
+                                                  valid_candidates[depth], valid_candidates_count,
+                                                  temp_buffer, temp_count);
+
+        std::swap(temp_buffer, valid_candidates[depth]);
+        valid_candidates_count = temp_count;
+    }
+
+    idx_count[depth] = valid_candidates_count;
+}
 
 int main(int argc, char** argv) {
     MatchingCommand command(argc, argv);
@@ -498,7 +803,7 @@ int main(int argc, char** argv) {
 
     // Sort the candidates to support the set intersections
     if (input_filter_type != "CECI")
-        FilterVertices::sortCandidates(candidates, candidates_count, query_graph->getVerticesCount());
+        sortCandidates(candidates, candidates_count, query_graph->getVerticesCount());
 
     end = std::chrono::high_resolution_clock::now();
     double filter_vertices_time_in_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
@@ -582,34 +887,8 @@ int main(int argc, char** argv) {
 
     start = std::chrono::high_resolution_clock::now();
 
-    if (input_engine_type == "EXPLORE") {
-        embedding_count = EvaluateQuery::exploreGraph(data_graph, query_graph, edge_matrix, candidates,
-                                                      candidates_count, matching_order, pivots, output_limit, call_count);
-    } else if (input_engine_type == "LFTJ") {
-        embedding_count = EvaluateQuery::LFTJ(data_graph, query_graph, edge_matrix, candidates, candidates_count,
-                                              matching_order, output_limit, call_count);
-    } else if (input_engine_type == "GQL") {
-        embedding_count = EvaluateQuery::exploreGraphQLStyle(data_graph, query_graph, candidates, candidates_count,
-                                                             matching_order, output_limit, call_count);
-    } else if (input_engine_type == "QSI") {
-        embedding_count = EvaluateQuery::exploreQuickSIStyle(data_graph, query_graph, candidates, candidates_count,
-                                                             matching_order, pivots, output_limit, call_count);
-    }
-    else if (input_engine_type == "DPiso") {
-        embedding_count = EvaluateQuery::exploreDPisoStyle(data_graph, query_graph, dpiso_tree,
-                                                           edge_matrix, candidates, candidates_count,
-                                                           weight_array, dpiso_order, output_limit,
-                                                           call_count);
-//        embedding_count = EvaluateQuery::exploreDPisoRecursiveStyle(data_graph, query_graph, dpiso_tree,
-//                                                           edge_matrix, candidates, candidates_count,
-//                                                           weight_array, dpiso_order, output_limit,
-//                                                           call_count);
-    }
-    else if (input_engine_type == "Spectrum") {
-        spectrum_analysis(data_graph, query_graph, edge_matrix, candidates, candidates_count, output_limit, spectrum, time_limit);
-    }
-    else if (input_engine_type == "CECI") {
-        embedding_count = EvaluateQuery::exploreCECIStyle(data_graph, query_graph, ceci_tree, candidates, candidates_count, TE_Candidates,
+    if (input_engine_type == "CECI") {
+        embedding_count = exploreCECIStyle(data_graph, query_graph, ceci_tree, candidates, candidates_count, TE_Candidates,
                 NTE_Candidates, ceci_order, output_limit, call_count);
     }
     else {
--- sub_ceci/myceci.h
+++ sub_ceci/myceci.h
@@ -1,11 +1,26 @@
+#include "graph/graph.h"
+#include <vector>
 
-VertexID selectCECIStartVertex(const Graph *data_graph, const Graph *query_graph);
-void generateCECIFilterPlan(const Graph *data_graph, const Graph *query_graph, TreeNode *&tree, VertexID *&order);
-void bfsTraversal(const Graph *graph, VertexID root_vertex, TreeNode *&tree, VertexID *&bfs_order);
-void allocateBuffer(const Graph *data_graph, const Graph *query_graph, ui **&candidates, ui *&candidates_count);
-void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex, ui &count, ui *buffer);
-bool CECIFilter(const Graph *data_graph, const Graph *query_graph, ui **&candidates, ui *&candidates_count,
-                           ui *&order, TreeNode *&tree,  std::vector<std::unordered_map<VertexID, std::vector<VertexID >>> &TE_Candidates,
-                           std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates);
-void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex, ui &count, ui *buffer);
-void generateCECIQueryPlan(const Graph *query_graph, TreeNode *tree, ui *bfs_order, ui *&order, ui *&pivot);
+
+    VertexID selectCECIStartVertex(const Graph *data_graph, const Graph *query_graph);
+    void generateCECIFilterPlan(const Graph *data_graph, const Graph *query_graph, TreeNode *&tree, VertexID *&order);
+    void bfsTraversal(const Graph *graph, VertexID root_vertex, TreeNode *&tree, VertexID *&bfs_order);
+    void allocateBuffer(const Graph *data_graph, const Graph *query_graph, ui **&candidates, ui *&candidates_count);
+    void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex, ui &count, ui *buffer);
+    bool CECIFilter(const Graph *data_graph, const Graph *query_graph, ui **&candidates, ui *&candidates_count,
+                            ui *&order, TreeNode *&tree,  std::vector<std::unordered_map<VertexID, std::vector<VertexID >>> &TE_Candidates,
+                            std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates);
+    void generateCECIQueryPlan(const Graph *query_graph, TreeNode *tree, ui *bfs_order, ui *&order, ui *&pivot);
+    void computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex,
+                                            ui &count, ui *buffer = NULL);
+    static size_t exploreCECIStyle(const Graph *data_graph, const Graph *query_graph, TreeNode *tree, ui **candidates,
+                                    ui *candidates_count,
+                                    std::vector<std::unordered_map<VertexID, std::vector<VertexID>>> &TE_Candidates,
+                                    std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates,
+                                    ui *order, size_t &output_limit_num, size_t &call_count);
+    void compactCandidates(ui **&candidates, ui *&candidates_count, ui query_vertex_num);
+    void generateValidCandidates(ui depth, ui *embedding, ui *idx_count, ui **valid_candidates, ui *order,
+                                            ui *&temp_buffer, TreeNode *tree,
+                                            std::vector<std::unordered_map<VertexID, std::vector<VertexID>>> &TE_Candidates,
+                                            std::vector<std::vector<std::unordered_map<VertexID, std::vector<VertexID>>>> &NTE_Candidates);
+    void sortCandidates(ui **candidates, ui *candidates_count, ui num);
