4aeca5ccdbaccd75f28158cc821cd9484740c05e
KeweiZhang <zhangkewei1994@gmail.com>
2022-01-04 17:39:32 +0800

    clean the directory.

--- scripts/run_webgoogle.sh
+++
@@ -1,5 +0,0 @@
-../ceci ../datasets/webgoogle.graph ../querysets/qg1
-../ceci ../datasets/webgoogle.graph ../querysets/qg2
-../ceci ../datasets/webgoogle.graph ../querysets/qg3
-../ceci ../datasets/webgoogle.graph ../querysets/qg4
-../ceci ../datasets/webgoogle.graph ../querysets/qg5
